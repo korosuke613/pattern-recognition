90496839f0dfe904dd91b06ce5885fcee297f95d
Futa Hirakoba <korosuke613613@gmail.com>
2018-07-12 11:02:54 +0900

    finish Sp

--- statistics_cy.pyx
+++ statistics_cy.pyx
@@ -71,51 +71,33 @@ cdef class St:
     cpdef double get(self):
         return 1 / self.hT()
 
-
 cdef class Sp:
     cdef tuple y
     cdef list n
     cdef int n1, n2
     cdef double sigma, x_probability
 
-    def __init__(self, y: tuple, n1=3, n2=3, sigma=1.0, x_probability=X[1][1]):
+    def __init__(self, tuple y, int n1=3, int n2=3, double sigma=1.0):
         self.y = y
         self.n1 = n1
         self.n2 = n2
         self.sigma = sigma
-        self.x_probability = x_probability
         self.n = [self.n1, self.n2]
 
-    cdef double pyx(self, int x, list yi):
-        cdef double result = 0.0
-        for y in yi:
-            result += exp(-((y - x) ** 2) / (2 * self.sigma ** 2)) / (sqrt(2 * M_PI) * self.sigma)
-        return result
-
-    cdef double _prxyx(self, list y):
-        cdef double result = 0.0
-        for i in range(2):
-            result += (X[i][0] + X[i][1])*self.pyx(1, y)
-        return result
-
-    cdef double prxy(self, list y1):
-        return ((X[1][0] + X[1][1]) * self.pyx(1, y1)) / self._prxyx(y1)
-
-    cdef double _prxxyx(self, list y):
-        return X[1][1] * self.pyx(1, y)
-
-    cdef double _prxxyx_sum(self, list y):
-        cdef double result = 0.0
-        for i in range(2):
-            result += X[1][i] * self.pyx(i, y)
-        return result
-
-    cdef double prxxy(self, list y2):
-        return self._prxxyx(y2) / self._prxxyx_sum(y2)
-
     cpdef double get(self):
-        return self.prxy(self.y[0]) * self.prxxy(self.y[1])
-
+        return 1.0 / self.invert()
+    cdef double invert(self):
+        return self.prX1GivenY1(self.n) * self.prX2GivenY2(self.n)
+    cdef double prX1GivenY1(self, list n_list):
+        return 1 + ((X[0][0] + X[0][1]) / (X[1][0] + X[1][1]) * exp(self.inExp(n_list, 0)))
+    cdef double prX2GivenY2(self, list n_list):
+        return 1 + (X[1][0] / X[1][1] * exp(self.inExp(n_list, 1)))
+    cdef double inExp(self, list n_list, int i):
+        cdef double w = 0.0
+        cdef int j
+        for j in range(n_list[i]):
+            w += (1.0 - 2.0 * self.y[i][j]) / (2.0 * self.sigma * self.sigma)
+        return w
 
 if __name__ == '__main__':
     data = DataSets(3, 3)
